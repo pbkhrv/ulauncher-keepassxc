0cf148eaa1bd79d0ea21563300af65291fc55fca
Peter Bakhirev <pbkhrv@pm.me>
2019-12-05 15:33:16 -0500

    remove unused type-check violating functions from wmctrl

--- keepassxc/wmctrl.py
+++ keepassxc/wmctrl.py
@@ -2,8 +2,7 @@
 Wrapper for the `wmctrl` command line utility
 """
 import subprocess
-import re
-from typing import NamedTuple
+from typing import Tuple
 
 
 class WmctrlNotFoundError(Exception):
@@ -12,39 +11,14 @@ class WmctrlNotFoundError(Exception):
     """
 
 
-class WindowInfo(NamedTuple):
-    """
-    Information about windows that wmctrl returns
-    """
-
-    id: str
-    desktop_id: int
-    pid: int
-    class_name: str
-    host: str
-    title: str
-
-
-def get_windows():
-    """
-    Request window info from wmctrl and parse results into list of WindowInfo objects
-    """
-    _, output = _run_wmctrl("-p", "-x", "-l")
-    windows = []
-    for line in output.splitlines():
-        line = re.sub(r" +", " ", line)
-        windows.append(WindowInfo(*line.split(" ", maxsplit=5)))
-    return windows
-
-
-def activate_window_by_id(window_id):
+def activate_window_by_id(window_id) -> None:
     """
     Execute wmctrl command "activate window by id"
     """
     _run_wmctrl("-i", "-a", window_id)
 
 
-def activate_window_by_class_name(class_name):
+def activate_window_by_class_name(class_name) -> None:
     """
     Execute wmctrl command "activate window by class name"
     """
@@ -52,7 +26,7 @@ def activate_window_by_class_name(class_name):
 
 
 # adapted from https://github.com/autokey/autokey/blob/master/lib/autokey/scripting.py
-def _run_wmctrl(*args):
+def _run_wmctrl(*args) -> Tuple[int, str]:
     """
     Execute wmctrl with given args and handle errors
     """
